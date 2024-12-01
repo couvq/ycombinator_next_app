a74a6e78a45533b1129e3e19b846d4e3a9c9dfd8
couvq <quentincouvelaire@gmail.com>
2024-11-30 19:14:21 -0500

    added server side data fetching to homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,26 @@
 import Hello from "../components/hello";
 
-export default function Home() {
-  console.log("What am I?");
+type Album = {
+  id: number;
+  title: string;
+};
+
+const Home = async () => {
+  // fetches data on server side and returns already put together page with the data already included
+  // --> improves first contentful paint, SEO, and security
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+  if (!response.ok) throw new Error("Failed to fetch data.");
+  const albums = await response.json();
+
   return (
     <>
       <h1 className="text-3xl">Welcome to Next.js</h1>
       <Hello />
+      {albums.map((album: Album) => (
+        <div key={album.id}>{album.title}</div>
+      ))}
     </>
   );
-}
+};
+
+export default Home;
